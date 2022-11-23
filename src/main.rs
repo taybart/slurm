6f593d84f895ab1b1f12e8c18df4e1a45da610bf
Taylor Bartlett <taybart@gmail.com>
2022-11-22 19:43:27 -0700

    feat: i guess support blobs

--- src/main.rs
+++ src/main.rs
@@ -79,9 +79,9 @@ fn validate_url(input: &String) -> Result<(Url, String), String> {
 
     let slugs: Vec<&str> = url.path_segments().unwrap().collect();
 
-    if slugs[2] != "tree" {
+    if slugs[2] != "tree" || slugs[2] != "blob" {
         // TODO: github only
-        return Err("malformed url, expected tree in path name".to_string());
+        return Err("malformed url, expected tree or blob in path name".to_string());
     }
 
     // let repo = segments[0..2].join("/");
